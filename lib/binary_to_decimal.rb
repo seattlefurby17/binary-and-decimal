181425b888223e677127a51055722554ea2fb09b
Hanh Solo <minhhanhnyc@gmail.com>
2020-09-16 13:09:17 -0700

    binary-and-decimal submission

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,7 +4,20 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
 def binary_to_decimal(binary_array)
+  sum_of_digits = 0
+  power_of_digit = 7
+  binary_array.each do |digit|
+  sum_of_digits += digit * (2**power_of_digit)
+  power_of_digit -= 1
+  end
+  return sum_of_digits
   raise NotImplementedError
 end
-# this iss
+
+puts binary_to_decimal([0, 0, 0, 0, 0, 0, 0, 0])
+puts binary_to_decimal([0, 0, 0, 0, 0, 0, 0, 1])
+puts binary_to_decimal([1, 0, 0, 0, 0, 0, 0, 1])
+puts binary_to_decimal([0, 0, 0, 0, 0, 0, 1, 1])
+
